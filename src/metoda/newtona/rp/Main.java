1991d2c5619537d2d2a2b29742283c8dfb85f7e1
Gnex <gnex@vivaldi.net>
2017-03-21 14:56:49 +0100

    no message

--- src/metoda/newtona/rp/Main.java
+++ src/metoda/newtona/rp/Main.java
@@ -1,8 +1,25 @@
 package metoda.newtona.rp;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Punkt> punkty = new ArrayList<>();
+        double podanyX, podanyY;
+        int iloscPunktow;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ile punktów chcesz wprowadzić?");
+        iloscPunktow = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < iloscPunktow; i++) {
+            System.out.println("Podaj wartość X:");
+            podanyX = Double.parseDouble(sc.nextLine());
+            System.out.println("Podaj wartość Y:");
+            podanyY = Double.parseDouble(sc.nextLine());
+            punkty.add(new Punkt(podanyX,podanyY));
+            System.out.println("Punkt numer " + (i+1) + " został dodany!");
+        }
     }
 }
--- src/metoda/newtona/rp/Punkt.java
+++ src/metoda/newtona/rp/Punkt.java
@@ -4,10 +4,10 @@ package metoda.newtona.rp;
  * Created by Gnex on 2017-03-21.
  */
 public class Punkt {
-    int x;
-    int y;
+    double x;
+    double y;
 
-    Punkt(int x, int y) {
+    Punkt(double x, double y) {
         this.x = x;
         this.y = y;
     }
