f6ecfda27352f47046876c97cb2d940e52e19774
Gnex <gnex@vivaldi.net>
2017-03-21 14:52:33 +0100

    no message

---
+++ src/metoda/newtona/rp/Punkt.java
@@ -0,0 +1,14 @@
+package metoda.newtona.rp;
+
+/**
+ * Created by Gnex on 2017-03-21.
+ */
+public class Punkt {
+    int x;
+    int y;
+
+    Punkt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
