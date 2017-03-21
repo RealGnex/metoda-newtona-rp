9df74ff2da4d8079350259cf08312c7b66bf1317
Gnex <gnex@vivaldi.net>
2017-03-21 15:52:38 +0100

    no message

--- src/metoda/newtona/rp/Main.java
+++ src/metoda/newtona/rp/Main.java
@@ -21,6 +21,7 @@ public class Main {
             punkty.add(new Punkt(podanyX,podanyY));
             System.out.println("Punkt numer " + (i+1) + " został dodany!");
         }
+        interpolacjaNewtona(2.5,punkty);
     }
     public static int silnia (int n) {
         if (n==0)
@@ -29,7 +30,7 @@ public class Main {
             return (n*silnia(n-1));
     }
 
-    public static double[] roznicaProgresywna(ArrayList<Punkt> punkty, double szukanyX) {
+    public static double[] roznicaProgresywna(ArrayList<Punkt> punkty) {
         double wynik = 0;
         final int iloscPunktow = punkty.size();
         double[][] roznicaProgresywna = new double[iloscPunktow][]; //tworzenie kolumn
@@ -51,5 +52,13 @@ public class Main {
 
     }
 
+    private static void interpolacjaNewtona(double szukanyX, ArrayList<Punkt> punkty) {
+        final int iloscPunktow = punkty.size();
+        double[] roznica = roznicaProgresywna(punkty);
+        double y = roznica[iloscPunktow-1];
+        for (int j = iloscPunktow - 2; j >= 0; j--)
+            y = y * (szukanyX - punkty.get(j).x) + roznica[j];
+        System.out.println("Interpolacja w punkcie " + szukanyX + " to " + y);
+    }
 
 }
